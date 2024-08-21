a86a44220883529822aaee7d00ce61a26c63e88e
huasong.chai <huasong.chai@zeekrlife.com>
2024-08-21 14:22:48 +0800

    modify ttf

--- learn/zeekr/jianbao.py
+++ learn/zeekr/jianbao.py
@@ -1,6 +1,6 @@
 """
 https://ai-bot.cn/daily-ai-news/
-counter=1; for file in *.png; do base=$(basename "$file" .png) new_name=$(printf "%d" "$counter") new_name="$new_name.png" mv -v -- "$file" "$new_name" ((counter++)) done
+counter=1; for file in *.png; do base=$(basename "$file" .png); new_name=$(printf "%d" "$counter"); new_name="$new_name.png"; mv -v -- "$file" "$new_name"; ((counter++)); done
 """
 
 import re
@@ -12,7 +12,7 @@ import shutil
 def add_text_to_image(image_path, title, subtitle_content_pairs, output_image_path,
                       title_font_size=32, subtitle_font_size=24, text_font_size=20,
                       text_color=(255, 255, 255), bg_color=(0, 0, 0, 128),
-                      padding=22, line_spacing=15, max_line_length=56, content_max_line_length=63):
+                      padding=18, line_spacing=15, max_line_length=56, content_max_line_length=64):
     # 打开背景图片
     image = Image.open(image_path).convert("RGBA")
     overlay = Image.new("RGBA", image.size, (255, 255, 255, 0))
@@ -21,9 +21,9 @@ def add_text_to_image(image_path, title, subtitle_content_pairs, output_image_pa
     draw = ImageDraw.Draw(overlay)
 
     # 加载字体
-    title_font = ImageFont.truetype("STHeiti Medium.ttc", title_font_size)
-    subtitle_font = ImageFont.truetype("STHeiti Medium.ttc", subtitle_font_size)
-    text_font = ImageFont.truetype("Hiragino Sans GB.ttc", text_font_size)
+    title_font = ImageFont.truetype("/Users/chaihuasong/Documents/AI/code/AIClub/learn/data/zeekr/ttf/lianxiangbold.ttf", title_font_size)
+    subtitle_font = ImageFont.truetype("/Users/chaihuasong/Documents/AI/code/AIClub/learn/data/zeekr/ttf/lianxiangbold.ttf", subtitle_font_size)
+    text_font = ImageFont.truetype("/Users/chaihuasong/Documents/AI/code/AIClub/learn/data/zeekr/ttf/lianxiang.ttf", text_font_size)
 
     # 获取图片尺寸
     image_width, image_height = image.size
@@ -75,7 +75,7 @@ def add_text_to_image(image_path, title, subtitle_content_pairs, output_image_pa
     ) + padding
 
     # 设置文本位置（垂直居中）
-    current_y = (image_height - total_text_height) / 2 - line_spacing * 4
+    current_y = (image_height - total_text_height) / 2 - line_spacing * 9 - 10
 
     # 计算背景矩形的大小
     max_text_width = max(
@@ -86,7 +86,7 @@ def add_text_to_image(image_path, title, subtitle_content_pairs, output_image_pa
         ) for subtitle_lines, content_lines in processed_pairs]
     )
     bg_width = max_text_width + 2 * padding
-    bg_height = total_text_height * 3 / 2 - len(processed_pairs) * padding * 2 + line_spacing
+    bg_height = total_text_height * 3 / 2 - len(processed_pairs) * 42 + line_spacing
 
     # 绘制半透明背景矩形
     bg_x = (image_width - bg_width) / 2
@@ -124,24 +124,31 @@ title = f"{today_date} 前沿播报"
 
 # 小标题和正文内容的动态输入
 subtitle_content_pairs = [
-    ("全球最强数学大模型 Qwen2-Math 发布人人可玩Demo",
-     "阿里千问大模型团队发布了 Qwen2-Math 的体验Demo，HuggingFace在线可玩。惊喜的是，如果嫌输入数学公式比较麻烦，可以把想问的题截图or扫描，上传即可解题。试玩界面的OCR功能，由阿里千问大模型团队Qwen2-VL提供支持；数学推理能力，由Qwen2-Math支持。"),
+    ("谷歌开放 HeAR AI 模型 API:1亿条咳嗽声训练，辅助筛查、诊断和监测肺结核",
+     "谷歌公司于8月19 日发布博文，宣布通过 Google Cloud API，目前已经向研究人员开放健康声学表征（HealthAcoustic Representations，简称 HeAR）AI 模型。谷歌 HeAR AI 模型可以帮助人类诊断疾病，可以通过分析人的咳嗽和呼吸，诊断出疾病。"),
 
-    ("Luma Dream Machine v1.5版本发布，更快、更真、更清晰",
-     "据 Luma AI 官方消息， Luma Dream Machine v1.5版本已经在近日发布，在视频生成方面获得巨大改善。据Luma官方表示，Luma Dream Machine V1.5版本在视频生成方面取得了显著进步，不仅提升了视频的质量，更缩短了生成时间。现在，用户可以在更短的时间内，得到更高质量的视频作品。"),
+    ("Salesforce推出 xGen-MM 开源多模态AI模型",
+     "xGen-MM 是Salesforce推出的一款开源多模态AI模型，具有处理交错数据的能力，能同时理解和生成文本、图像等多种数据类型。xGen-MIM 通过学习大量的图片和文字信息，不仅在视觉语言任务上展现出强大的性能，还通过开源模型、数据集和微调代码库，促进模型能力的不断提升。"),
 
-    ("AMD 宣布49亿美元收购服务器制造商 ZT Systems 以挑战英伟达",
-     "AMD 宣布同意以75%现金和 25%股票交易方式收购服务器制造商 ZT Systems，交易价值为49亿美元，以增加数据中心技术。ZT Systems 将成为 AMD 数据中心解决方案业务集团的一部分。ZT Systems 在过去12个月的收入超过100亿美元。"),
+    ("OpenAI 开放GPT-40微调功能，企业可更轻松打造专属AI助手",
+     "OpenAI 推出了一项新功能，允许企业客户使用自己的数据来定制其最强大的 AI 模型 GPT-40。此举旨在应对日益激烈的AI企业应用竞争，并满足企业对 AI投资回报的更高要求。通过微调，现有 AI 模型可以针对特定任务或领域进行优化。"),
 
-    ("通义千问宣布启用新域名\"tongyi.ai\"，网页版聊天新增深度搜索功能",
-     "阿里大语言模型“通义千问”今日宣布启用新域名“tongyi.ai”，并带来多项新功能。网页版聊天新增深度搜索功能；App 图片微动效支持多尺寸图片；App 自定义唱演支持 3:4 画幅（原先 1:1）。")
+    ("PICO发布“中国版Vision Pro”，搭载的AI芯片性能暴增800%",
+     "8月20日下午，字节跳动旗下XR平台PICO推出首款MR混合现实一体机PICO 4 Ultra，硬件上搭载全新高通骁龙XR2 Gen2计算平台，拥有12GB超大内存，GPU性能相比前代XR1提升2.5倍，Al性能比XR1提升8倍。价格方面，PICO 4 Ultra消费者版本售价4299元，PICO 4 Ultra Enterprise（企业版）7499元，PICO体感追踪器售价399元一对，现已全面开启预售"),
+
+    ("iPad上最强的绘画应用 Procreate，永远不会在其产品中引入生成式AI",
+     "Procreate CEO James Cuda 宣布该应用将永不使用生成式AI技术，以保护艺术家免受其影响；Procreate 是一款受欢迎的iPad绘图应用，自2011年上线以来获得多个奖项，并广泛用于艺术和设计教育；尽管Procreate拒绝采用生成式Al，公司仍将继续使用传统的机器学习技术来优化应用功能。"),
+
+    ("EliseAI 跻身纽约独角兽行列：D轮融资7500万美元、估值超10亿美元",
+     "据VentureBeat官网报道，近日，房产科技公司 EliseAI宣布成功完成7500万美元D轮融资，此轮融资由知名风投公司 Sapphire Ventures 领投，新筹集的资金将主要用于扩充团队规模，推进产品研发。至此，EliseAI的估值超过10亿美元，成为了纽约最新的独角兽公司。")
+
 ]
 
 # 生成1到80之间的随机整数
 random_number = random.randint(1,  80)
 print(random_number)
 # 背景图片路径和输出图片路径
-background_img_path = '/Users/chaihuasong/Documents/AI/resources/pictures/' + str(random_number) + ".png"
+background_img_path = '/Users/chaihuasong/Documents/AI/resources/pic2/' + str(random_number) + ".png"
 output_image_path = '../../learn/data/zeekr/img/' + str(random_number) + ".png"
 
 print(output_image_path)
